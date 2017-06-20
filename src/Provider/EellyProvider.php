517ed34f0218661da352d40689f2c090ba4af090
moqiong <moqun@tom.com>
2017-06-20 11:17:11 +0800

    初始上传

---
+++ src/Provider/EellyProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * PHP version 7.1
+ *
+ * @copyright Copyright (c) 2012-2017 EELLY Inc. (https://www.eelly.com)
+ * @link      https://api.eelly.com
+ * @license   衣联网版权所有
+ */
+
+namespace Eelly\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\GenericProvider;
+
+/**
+ * @author hehui<hehui@eelly.net>
+ */
+class EellyProvider extends GenericProvider
+{
+}
