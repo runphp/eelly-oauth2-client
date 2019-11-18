99d944d8e16d8fac9e04f9d8705fbf4bcc41fcd9
hehui <runphp@qq.com>
2019-11-18 20:30:11 +0800

    修改缓存为phpfile

--- src/Tool/AccessTokenCacheTrait.php
+++ src/Tool/AccessTokenCacheTrait.php
@@ -15,6 +15,7 @@ namespace Shadon\OAuth2\Client\Tool;
 
 use League\OAuth2\Client\Grant\Exception\InvalidGrantException;
 use Phalcon\Cache\BackendInterface as CacheInterface;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 
 /**
  * @author hehui<hehui@eelly.net>
@@ -46,15 +47,20 @@ trait AccessTokenCacheTrait
         if (false != $key) {
             array_push($params, $key);
         }
-        $keyName = $this->keyName(__CLASS__, __FUNCTION__, $params);
-        $accessToken = $this->cache->get($keyName);
-        if (empty($accessToken) || $accessToken->hasExpired()) {
+        $keyName = $this->keyName('AccessTokenCacheTrait', __FUNCTION__, $params);
+        $cachePool = new PhpFilesAdapter('eelly', 0, 'var/cache');
+        $cacheItem = $cachePool->getItem($keyName);
+        if ($cacheItem->isHit()) {
+            $accessToken = $cacheItem->get();
+        } else {
             try {
                 $accessToken = parent::getAccessToken($grant, $options);
             } catch (InvalidGrantException $e) {
                 $accessToken = $this->getCustomAccessToken($grant, $options);
             }
-            $this->cache->save($keyName, $accessToken, $accessToken->getExpires() - time() - 10 /* 提前10秒过期 */);
+            $cacheItem->set($accessToken);
+            $cacheItem->expiresAfter($accessToken->getExpires() - time() - 10 /* 提前10秒过期 */);
+            $cachePool->save($cacheItem);
         }
 
         return $accessToken;
@@ -80,23 +86,6 @@ trait AccessTokenCacheTrait
      */
     private function keyName($class, $method, array $params)
     {
-        return sprintf('%s:%s:%s', $class, $method, $this->createKeyWithArray($params));
-    }
-
-    private function createKeyWithArray(array $parameters)
-    {
-        $uniqueKey = [];
-
-        foreach ($parameters as $key => $value) {
-            if (is_scalar($value)) {
-                $uniqueKey[] = $key.':'.$value;
-            } elseif (\is_array($value)) {
-                $uniqueKey[] = $key.':['.$this->createKeyWithArray($value).']';
-            } else {
-                throw new \InvalidArgumentException('can not use cache annotation', 500);
-            }
-        }
-
-        return implode(',', $uniqueKey);
+        return sprintf('%s_%s_%s', $class, $method, md5(json_encode($params)));
     }
 }
